ad145f47d6e9e7045d5264841e8a3456ee9e0c69
Пользователь <lilu163@mail.ru>
2024-05-22 14:50:32 +0400

    refresh

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// noinspection JSNonASCIINames
 import {firstPred, pred} from "./data.js"
 'use strict'
 const head= document.querySelector("#heading");
@@ -8,12 +7,12 @@ const pText= document.querySelector("#p-text");
 pText.textContent = firstPred.join("");
 let textPred = '';
 function getPred() {
-    let index = Math.floor(Math.random() * pred.length);
-        textPred = pred[index];
-    pText.textContent = textPred;
-    head.textContent = " ";
-    pContent.setAttribute("style", "border: 5px solid #0dcaf0");
-    pText.setAttribute("style", "margin-top: 50px");
+  let index = Math.floor(Math.random() * pred.length);
+  textPred = pred[index];
+  pText.textContent = textPred;
+  head.textContent = " ";
+  pContent.setAttribute("style", "border: 5px solid #0dcaf0");
+  pText.setAttribute("style", "margin-top: 50px");
 }
 
 btn.addEventListener("click", getPred);
